76a73c189f7424c07af5a2bf9de7b6bb0d18fa34
Taras Melnychuk <melnychuktaras@gmail.com>
2014-11-26 14:47:19 +0200

    Clean up utils module (pep8)

--- collective/contentfiles2aws/utils.py
+++ collective/contentfiles2aws/utils.py
@@ -4,19 +4,24 @@ from collective.contentfiles2aws import config
 from collective.contentfiles2aws.awsfile import AWSFile
 from collective.contentfiles2aws.interfaces import IAWSField
 
+
 def mark_request(request, name, value):
     annotations = IAnnotations(request)
     annotations[name] = value
 
+
 def abort_transaction(request):
     mark_request(request, config.ABORT_TRANSACTION_FLAG, 1)
+
 
 def skip_source_remove(request):
     mark_request(request, config.SKIP_SOURCE_REMOVE_FLAG, 1)
 
+
 def replace_source_uid(sid, uid):
     old_uid = sid.split('_')[0]
-    return  sid.replace(old_uid, uid)
+    return sid.replace(old_uid, uid)
+
 
 def getAWSFields(obj):
     """ Collect aws fields for provided object.
@@ -30,7 +35,7 @@ def getAWSFields(obj):
     aws_fields = []
     for f in obj.schema.fields():
         if IAWSField.providedBy(f):
-            accessor= f.getAccessor(obj)
+            accessor = f.getAccessor(obj)
             field_content = accessor()
             if isinstance(field_content, AWSFile):
                 aws_fields.append((f, field_content))
